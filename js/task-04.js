e4830ffe62fdee29beaebc4b3990c659abd741b5
Oleksii Leschenko <djlabuh@gmail.com>
2023-02-26 21:47:40 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+let value = 0;
+
+const counterValue = document.querySelector('#value');
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+
+const handleDecrementClick = () => {
+  value -= 1;
+  counterValue.textContent = value;
+};
+
+const handleIncrementClick = () => {
+  value += 1;
+  counterValue.textContent = value;
+};
+
+
+decrementButton.addEventListener('click', handleDecrementClick);
+incrementButton.addEventListener('click', handleIncrementClick);
